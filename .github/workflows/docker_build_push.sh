6b1cc066c4f978c9569aa7efd7bea2d1dfd6e730
Silvia Rocha <silvia.rocha@dsr-corporation>
2022-11-07 13:53:33 +0000

    Adapted github docker workflows

--- .github/workflows/docker_build_push.sh
+++ .github/workflows/docker_build_push.sh
@@ -18,7 +18,7 @@
 set -eo pipefail
 
 SHA=$(git rev-parse HEAD)
-REPO_NAME="apache/superset"
+REPO_NAME="dsr/superset"
 
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
   REFSPEC=$(echo "${GITHUB_HEAD_REF}" | sed 's/[^a-zA-Z0-9]/-/g' | head -c 40)
@@ -32,7 +32,7 @@ else
   LATEST_TAG="${REFSPEC}"
 fi
 
-if [[ "${REFSPEC}" == "master" ]]; then
+if [[ "${REFSPEC}" == "rm-stable" ]]; then
   LATEST_TAG="latest"
 fi
 
@@ -43,38 +43,25 @@ cat<<EOF
   - ${REPO_NAME}:${LATEST_TAG}
 EOF
 
-#
-# Build the "lean" image
-#
-docker build --target lean \
-  -t "${REPO_NAME}:${SHA}" \
-  -t "${REPO_NAME}:${REFSPEC}" \
-  -t "${REPO_NAME}:${LATEST_TAG}" \
-  --label "sha=${SHA}" \
-  --label "built_at=$(date)" \
-  --label "target=lean" \
-  --label "build_actor=${GITHUB_ACTOR}" \
-  .
 
+#docker build \
+#  -t "${REPO_NAME}:${SHA}" \
+#  -t "${REPO_NAME}:${REFSPEC}" \
+#  -t "${REPO_NAME}:${LATEST_TAG}" \
+#  --label "sha=${SHA}" \
+#  --label "built_at=$(date)" \
+#  --label "build_actor=${GITHUB_ACTOR}" \
+#  .
 #
-# Build the dev image
-#
-docker build --target dev \
-  -t "${REPO_NAME}:${SHA}-dev" \
-  -t "${REPO_NAME}:${REFSPEC}-dev" \
-  -t "${REPO_NAME}:${LATEST_TAG}-dev" \
-  --label "sha=${SHA}" \
-  --label "built_at=$(date)" \
-  --label "target=dev" \
-  --label "build_actor=${GITHUB_ACTOR}" \
-  .
 
 if [ -z "${DOCKERHUB_TOKEN}" ]; then
   # Skip if secrets aren't populated -- they're only visible for actions running in the repo (not on forks)
   echo "Skipping Docker push"
 else
   # Login and push
-  docker logout
-  docker login --username "${DOCKERHUB_USER}" --password "${DOCKERHUB_TOKEN}"
-  docker push --all-tags "${REPO_NAME}"
+docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_TOKEN} ${DOCKERHUB_URL}
+docker build --no-cache -t "${REPO_NAME}:${REFSPEC}" .
+docker tag "${REPO_NAME}:${REFSPEC}" "${REPO_NAME}:${LATEST_TAG}"
+docker push "${REPO_NAME}:${LATEST_TAG}"
+docker push "${REPO_NAME}:${REFSPEC}"
 fi
